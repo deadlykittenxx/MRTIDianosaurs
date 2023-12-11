6f641a2e5e1c199255a80d92a03b6dde815fa47b
deadlykittenxx <96yean@gmail.com>
2023-12-11 23:56:51 +0100

    scene division + transition

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:16a93190d843d8c4a5ff7412c4eb4349b3223d79b2f4dcd72c0a1ddfb1d7ca12
+size 575
--- Assets/Script/ImageTarget.cs
+++ Assets/Script/ImageTarget.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1d3889441e682c05e1ba3673cfa5ba8f6a37f1feb2a42ac4d769ca8f2d24163
-size 752
+oid sha256:fdf48f4c47174003c86a1e2180e774f6109174e4538212ccfea77557d08b7c03
+size 730
