ab1594d539ddb6b39a70ae44b5740313684b82bf
deadlykittenxx <96yean@gmail.com>
2023-12-14 03:46:05 +0100

    Velociraptor animation modification

--- Assets/Script/ColliTest.cs
+++ Assets/Script/ColliTest.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:018794e69bb7c84b555abe7b4c945e4b5d50db383d7892c173529a3c98d05bc1
-size 1200
+oid sha256:555347998dd3d06267bb8513185c00eefda11fc953cf420eb414aee1dcd47bf0
+size 1374
